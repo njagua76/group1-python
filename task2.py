03d16d15a4c238802cb7a23502bb11f3e1a6acf0
Ann Gathoni <theanngathoni@gmail.com>
2025-09-17 15:26:37 +0300

    initial commit

---
+++ task2.py
@@ -0,0 +1,8 @@
+'''
+Create a program whose major task is to calculate an individual’s Net Salary by getting the inputs basic salary and benefits. Calculate the payee (i.e. Tax), NHIFDeductions, NSSFDeductions, gross salary, and net salary.
+
+NB: Use KRA, NHIF, and NSSF values provided in the link below.
+https://www.aren.co.ke/payroll/taxrates.htm
+https://www.kra.go.ke/en/individual/calculate-tax/calculating-tax/paye
+
+'''
